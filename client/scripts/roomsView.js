55f98d552590861896648f9a4423b609bbafb231
Luke Eastman <LukeAEastman@gmail.com>
2020-08-28 16:49:25 -0700

    add room to DOM

--- client/scripts/roomsView.js
+++ client/scripts/roomsView.js
@@ -2,11 +2,15 @@ var RoomsView = {
 
   $button: $('#rooms button'),
   $select: $('#rooms select'),
+  $rooms: $('#rooms'),
 
   initialize: function() {
   },
 
   render: function() {
-  }
+  },
 
+  renderRoom: function(room) {
+    RoomsView.$select.append('<div></div>');
+  }
 };
